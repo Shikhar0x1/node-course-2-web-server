45a409c3e9442392aba5ced6611a4d285bc47a4a
Shikhar0x1 <aoe0x1@gmail.com>
2017-08-05 02:42:22 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    console.log(`${now}`);
+    fs.appendFile('server.log', now + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append File !')
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express !</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'HomePage',
+        welcomeMessage: 'Welcome to my Page'
+    });
+});
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    })
+});
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to load'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on Port 3000');
+});
